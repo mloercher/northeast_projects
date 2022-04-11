059bed5fc6afd83908fd7779a913e09362e5e8a4
Mark <markloercher@gmail.com>
2022-04-11 14:23:41 -0400

    added what is and fair model sections

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,6 @@ import '../App.css'
 function HeroSection() {
   return (
     <div className='hero-container'>
-        <img src='../public/images/hero.png' alt="hero"></img>
     </div>
   )
 }
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css'
+import './Home.css'
 import '../HeroSection.css'
 // import HeroSection from '../HeroSection';
 
@@ -7,10 +8,21 @@ import '../HeroSection.css'
 
 
 function Home() {
-    return(
-    <div className='hero-container'>
-        {/* <img src='../public/images/hero.png' alt="hero"></img> */}
-    </div>
+    return (
+        <>
+            <div className='hero-container'>
+                {/* <img src='../public/images/hero.png' alt="hero"></img> */}
+            </div>
+            <div className='what-is'>
+                <h1>What is Passive House?</h1>
+                <p>'Passive House' is a building methodology developed to aggressively meet the climate crisis carbon reduction imperative while making a comfortable, healthy and affordable built environment. It calls for drastic cuts in the amount of energy our buildings consume through passive methods (insulation, solar heat gain, etc) - hence the name 'Passive House'. Unlike many other 'green' certifications, the Passive House standard is performance driven, meaning you either pass or fail based on target criteria developed for your specific project. </p>
+            </div>
+            <div className='fair-share'>
+                <h1>Fair Share Principle</h1>
+                <p>'Passive House' is a building methodology developed to aggressively meet the climate crisis carbon reduction imperative while making a comfortable, healthy and affordable built environment. It calls for drastic cuts in the amount of energy our buildings consume through passive methods (insulation, solar heat gain, etc) - hence the name 'Passive House'. Unlike many other 'green' certifications, the Passive House standard is performance driven, meaning you either pass or fail based on target criteria developed for your specific project. </p>
+            </div>
+            <img id="model-fair" src='../../images/fairShare.png' alt='fair-share-model'></img>
+        </>
     )
 }
 
