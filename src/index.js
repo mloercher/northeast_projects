3329bba0afe794ebb299cfbf25956242192b1179
Mark <markloercher@gmail.com>
2022-04-13 18:36:06 -0400

    changed reactDOM import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // import { FontAwesomeIcon, } from "@fortawesome/react-fontawesome";
 import React from "react";
-import ReactDOM from "react-dom";
-import './App.css';
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
 
 // reportWebVitals();
