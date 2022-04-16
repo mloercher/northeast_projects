f182c5bab5b4bf0d138d6aa4a6618b27973ec26d
Mark <markloercher@gmail.com>
2022-04-16 18:46:53 -0400

    added css, rearranged card rows

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,101 +1,86 @@
 import React from 'react'
-import CardItem from './CardItem'
+// import CardItem from './CardItem'
 import './Cards.css'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Row } from 'react-bootstrap'
+
+
 
 
 function Cards() {
+
   return (
-    <div className='card-container'>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text className='description'>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-          {/* <Button variant="primary">View Project</Button> */}
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant="top" src="../images/accord_proj.png" />
-        <Card.Body>
-          <Card.Title>Accord Passive House</Card.Title>
-          <Card.Text>
-            2018 PHIUS Design Competition
-            Honorable Mention: Residential
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-  )
+    <div>
+      <Row>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/accord_proj.png" />
+          <Card.Body>
+            <Card.Title>Accord Passive House</Card.Title>
+            <Card.Text className='description'>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/newburgh_proj.png" />
+          <Card.Body>
+            <Card.Title>Newburgh Passive House</Card.Title>
+            <Card.Text>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/accord_proj.png" />
+          <Card.Body>
+            <Card.Title>Accord Passive House</Card.Title>
+            <Card.Text>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+      <Row>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/accord_proj.png" />
+          <Card.Body>
+            <Card.Title>Accord Passive House</Card.Title>
+            <Card.Text className='description'>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/newburgh_proj.png" />
+          <Card.Body>
+            <Card.Title>Newburgh Passive House</Card.Title>
+            <Card.Text>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant="top" src="../images/accord_proj.png" />
+          <Card.Body>
+            <Card.Title>Accord Passive House</Card.Title>
+            <Card.Text>
+              2018 PHIUS Design Competition
+              Honorable Mention: Residential
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+      </div>
+      )
 }
 
-export default Cards;
+      export default Cards;
 
-
-{/* <div className='cards'>
+      {/* <div className='cards'>
 <h1>Projects</h1>
  <div className='cards-container'>
     <div className='cards-wrapper'>
