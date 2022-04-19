cc5f7292328e6974ab9eb4710f00d5ff2218ccd6
Mark <markloercher@gmail.com>
2022-04-18 21:03:43 -0400

    created accord_slides doc and mapped with Accord page carousel

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,9 +7,9 @@ import project_data from './project_data'
 
 
 
-function Cards(props) {
+function Cards() {
   return project_data.map((project) => (
-    <div>
+    <>
       <Row>
         <Link to={project.link}>
           <Card>
@@ -17,13 +17,13 @@ function Cards(props) {
               <Card.Title>{project.title}</Card.Title>
               <Card.Text className='description'> {project.text}
               </Card.Text>
-              <Card.Img variant="top" src={project.img}/>
+              <Card.Img variant="top" src={project.img} />
             </Card.Body>
           </Card>
         </Link>
       </Row>
-    </div>
+    </>
   ));
-  }
+}
 
-  export default Cards;
+export default Cards;
---
+++ src/components/Pages/AccordSlides.js
@@ -0,0 +1,9 @@
+const accord_slides = [
+    {
+        img1:'../images/hero.png',
+        img2:'../images/fairShare.png',
+        img3:'../images/newburgh_proj.png'
+    }
+]
+
+export default accord_slides;
