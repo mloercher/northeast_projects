64876da6e9ceab2c106c049cacddd1790cdc8709
Mark <markloercher@gmail.com>
2022-04-19 13:08:35 -0400

    project cards in Rows of 3 items

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import CardItem from './CardItem'
 import './Cards.css'
-import { Card, Container, Row } from 'react-bootstrap'
+import { Card, Container, Row, Col} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import project_data from './Pages/project_data'
 
@@ -10,23 +10,24 @@ import project_data from './Pages/project_data'
 function Cards() {
   return (
     <Container>
+      <Row lg={3}>
       {project_data.map((project) => (
-        <>
           <Link to={project.link}>
-            <Card className=''>
-              <Card.Body className='card-body'>
+            <Card >
+              <Card.Body>
                 <Card.Title>{project.title}</Card.Title>
-                <Card.Text className='description'> {project.text}
+                <Card.Text className='text'> {project.text}
                 </Card.Text>
                 <Card.Img variant="top" src={project.img} />
               </Card.Body>
             </Card>
           </Link>
-        </>
       ))
       }
+      </Row>
     </Container>
   )
+  
 }
 
 export default Cards;
