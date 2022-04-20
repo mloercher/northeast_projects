3dd21b4c705e23735e8eb55eb97e8e4dc1b9c8bd
Mark <markloercher@gmail.com>
2022-04-20 13:35:59 -0400

    overlays

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import CardItem from './CardItem'
 import './Cards.css'
-import { Card, Container, Row} from 'react-bootstrap'
+import { Card, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import project_data from './Pages/project_data'
 
@@ -14,11 +14,16 @@ function Cards() {
         {project_data.map((project) => (
           <Link style={{ textDecoration: 'none' }} to={project.link}>
             <Card className="rounded-0 mt-4">
-              <img className="proj-img rounded-0 position-relative" alt="project" src={project.img}></img> 
+              <img src={project.img} className="proj-img rounded-0 position-relative" alt="project" ></img>
+              <Card.ImgOverlay className='title-overlay'>
+              <h5>{project.title}</h5>
+              </Card.ImgOverlay>
               <Card.ImgOverlay>
+              </Card.ImgOverlay>
+              {/* <Card.ImgOverlay className='proj-title'>
                 <h5>{project.title}</h5>
-                {/* <Card.Text> {project.text}</Card.Text> */}
-                </Card.ImgOverlay>
+                <Card.Text> {project.text}</Card.Text>
+              </Card.ImgOverlay> */}
             </Card>
           </Link>
         ))
