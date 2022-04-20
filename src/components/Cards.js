40d1bf1a19eab12b7da3b362b0a0985a24355f13
Mark <markloercher@gmail.com>
2022-04-20 11:18:47 -0400

    row gutter edit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import CardItem from './CardItem'
 import './Cards.css'
-import { Card, Container, Row, Col } from 'react-bootstrap'
+import { Card, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import project_data from './Pages/project_data'
 
@@ -10,16 +10,13 @@ import project_data from './Pages/project_data'
 function Cards() {
   return (
     <Container fluid>
-      <Row lg={3}>
+      <Row className='gy-4' lg={3}>
         {project_data.map((project) => (
-          <Link to={project.link}>
-            <Card >
-
+          <Link style={{textDecoration: 'none'}} to={project.link}>
+            <Card className="rounded-0 mt-5">
               <Card.Title>{project.title}</Card.Title>
-              <Card.Text className='text'> {project.text}
-              </Card.Text>
-              <Card.Img variant="top" src={project.img} />
-
+              <Card.Text> {project.text}</Card.Text>
+              <Card.Img className="proj-img"src={project.img} />
             </Card>
           </Link>
         ))
