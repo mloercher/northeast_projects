2ee35e870a123bc8fabc552629361e54747f2c77
Mark <markloercher@gmail.com>
2022-04-20 11:55:41 -0400

    added ImgOverlay to project cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import CardItem from './CardItem'
 import './Cards.css'
-import { Card, Container, Row } from 'react-bootstrap'
+import { Card, Container, Row} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import project_data from './Pages/project_data'
 
@@ -12,11 +12,13 @@ function Cards() {
     <Container fluid>
       <Row className='gy-4' lg={3}>
         {project_data.map((project) => (
-          <Link style={{textDecoration: 'none'}} to={project.link}>
-            <Card className="rounded-0 mt-5">
-              <Card.Title>{project.title}</Card.Title>
-              <Card.Text> {project.text}</Card.Text>
-              <Card.Img className="proj-img"src={project.img} />
+          <Link style={{ textDecoration: 'none' }} to={project.link}>
+            <Card className="rounded-0 mt-4">
+              <img className="proj-img rounded-0 position-relative" alt="project" src={project.img}></img> 
+              <Card.ImgOverlay>
+                <h5>{project.title}</h5>
+                {/* <Card.Text> {project.text}</Card.Text> */}
+                </Card.ImgOverlay>
             </Card>
           </Link>
         ))
