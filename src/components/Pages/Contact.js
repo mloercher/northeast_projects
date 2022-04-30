ab24db5abc337329e87cf0b1edf35a8fa28d042b
Mark <markloercher@gmail.com>
2022-04-29 20:50:55 -0400

    added services to contact page and added media query for sizing of home in  mobile view

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -11,28 +11,51 @@ function Contact() {
             </div>
             <div className='bottom-container'>
                 <div className='contact'>
-                {/* <h1>CONTACT</h1> */}
-                <div className='phone'>
-                    <FaPhoneSquareAlt size={50} />
-                    <p>518-555-5555</p>
+                    <h1 className='c-title'>CONTACT:</h1>
+                    <div className='phone'>
+                        <FaPhoneSquareAlt size={50} />
+                        <p>518-555-5555</p>
+                    </div>
+                    <div className='email'>
+                        <FaEnvelope size={50} />
+                        <p>john@ne-projects.com</p>
+                    </div>
                 </div>
-                <div className='email'>
-                    <FaEnvelope size={50} />
-                    <p>john@ne-projects.com</p>
-                </div>
-                </div>
+                <div className='appt'>
+                    <h1 className='c-title'>SCHEDULE A MEETING:</h1>
+                    <p className='services'>Services:
 
-                <PopupButton
-                    url="https://calendly.com/jmccasusi/"
-                    /*
-                     * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-                     * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-                     */
-                    rootElement={document.getElementById("root")}
-                    text="Click here to schedule!"
-                    textColor="#000000"
-                    color="#FFFFFF"
-                />
+                        <ul>
+                            <li>
+
+                                Building Performance Consulting
+                            </li>
+                            <li>
+
+                                Whole Building Energy Modeling
+                            </li>
+                            <li>
+
+                                Passive House Feasibility Study
+                            </li>
+                            <li>
+
+                                Passive House Design and Certification
+                            </li>
+                        </ul>
+                    </p>
+                    <PopupButton
+                        url="https://calendly.com/jmccasusi/"
+                        /*
+                         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+                         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+                         */
+                        rootElement={document.getElementById("root")}
+                        text="Click here to schedule!"
+                        textColor="#000000"
+                        color="#FFFFFF"
+                    />
+                </div>
             </div>
         </div>
     )
