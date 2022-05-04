9ce05c033413a613f148a102d39ee53930b46757
Mark <markloercher@gmail.com>
2022-05-04 18:47:52 -0400

    added accord project component

--- src/components/Pages/Accord.js
+++ src/components/Pages/Accord.js
@@ -1,50 +1,64 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap';
 import './Accord.css';
 // import accord_slides from './AccordSlides';
 
 function Accord() {
     // return accord_slides.map((slide) => (
     return (
-        <div>
-            <h1>Accord Passive House</h1>
-            <Carousel >
-                <Carousel.Item >
-                    <img
-                        className="d-block w-100"
-                        src='../images/hero.png'
-                        alt="First slide"
-                    />
-                    <Carousel.Caption className='cap'>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='../images/fairShare.png'
-                        alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='../images/newburgh_proj.png'
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+        <div className='accord-container'>
+            <div className='title-img'>
+                <img src='../images/accord_proj.png' alt='accord passive house' id='accord_proj'></img>
+                <h1 id='proj_name'>Accord Passive House</h1>
+                <h5 id='proj_location'>Accord, NY</h5>
+            </div>
+            <div className='accord-info'>
+                <div className='accord-stats'>
+                    <div className='category'>
+                        <p className='title'>Type:</p>
+                        <p className='stat'>Single-Family</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>iCFA</p>
+                        <p className='stat'>2,755 ft2</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Climate</p>
+                        <p className='stat'>5a</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Surface/iCFA</p>
+                        <p className='stat'>2.785</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Heating Demand</p>
+                        <p className='stat'>5.76 kBTU/ft2.yr</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Cooling Demand</p>
+                        <p className='stat'>.96 kBTU/ft2.yr</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Heating Load</p>
+                        <p className='stat'>4.31 BTU/hr.ft2</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Cooling Load</p>
+                        <p className='stat'>2.19 BTU/hr.ft2</p>
+                    </div>
+                    |
+                    <div className='category'>
+                        <p className='title'>Source Energy</p>
+                        <p className='stat'>5,441 kWh/ea.yr</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
     // ))
