503e37298f473f67f4ab9f12585bf223c9802b8b
Mark <markloercher@gmail.com>
2022-05-04 21:09:57 -0400

    added imgs to individual project pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import Projects from './components/Pages/Projects'
 import Accord from './components/Pages/Accord';
 import Footer from './components/Footer';
 import Contact from './components/Pages/Contact';
+import Newburgh from './components/Pages/Newburgh';
+import Gallatin from './components/Pages/Gallatin';
+import Hilltop from './components/Pages/Hilltop';
+import Dubois from './components/Pages/Dubois';
+import Pr from './components/Pages/Pr';
 
 function App() {
   return (
@@ -20,11 +25,16 @@ function App() {
       <NavBar />
       <div className="container-wrap">
         <Routes>
-          <Route exact path='/' element={<Home />}/>
+          <Route exact path='/' element={<Home />} />
           <Route exact path='/about' element={<About />} />
           <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/accord' element={<Accord />} />
+          <Route exact path='/newburgh' element={<Newburgh />} />
+          <Route exact path='/gallatin' element={<Gallatin />} />
+          <Route exact path='/hilltop' element={<Hilltop />} />
+          <Route exact path='/dubois' element={<Dubois />} />
+          <Route exact path='/pr' element={<Pr />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/components/Pages/Gallatin.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Accord.css';
+
+function Gallatin() {
+  return (
+    <div className='accord-container'>
+        <div className='title-img'>
+            <img src='../images/gallatin/TableOut.jpg' alt='accord passive house' id='accord_proj'></img>
+            <h1 id='proj_name'>Gallatin Passive House</h1>
+            <h5 id='proj_location'>Gallatin, NY</h5>
+        </div>
+        <div className='accord-info'>
+            <div className='accord-stats'>
+                <div className='category'>
+                    <p className='title'>Type:</p>
+                    <p className='stat'>Single-Family</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>iCFA</p>
+                    <p className='stat'>2,755 ft2</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Climate</p>
+                    <p className='stat'>5a</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Surface/iCFA</p>
+                    <p className='stat'>2.785</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Heating Demand</p>
+                    <p className='stat'>5.76 kBTU/ft2.yr</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Cooling Demand</p>
+                    <p className='stat'>.96 kBTU/ft2.yr</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Heating Load</p>
+                    <p className='stat'>4.31 BTU/hr.ft2</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Cooling Load</p>
+                    <p className='stat'>2.19 BTU/hr.ft2</p>
+                </div>
+                |
+                <div className='category'>
+                    <p className='title'>Source Energy</p>
+                    <p className='stat'>5,441 kWh/ea.yr</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default Gallatin
