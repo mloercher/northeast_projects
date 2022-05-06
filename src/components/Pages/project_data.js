2a6182903e451f073ad118b67d8d76f10c625a57
Mark <markloercher@gmail.com>
2022-05-06 10:50:43 -0400

    re-imported images in project_data

--- src/components/Pages/project_data.js
+++ src/components/Pages/project_data.js
@@ -1,44 +1,51 @@
+import accord_crop from '../../images/accord_crop.png'
+import newburgh_proj from '../../images/newburgh_proj.png'
+import sideEleBlast from '../../images/gallatin/sideEleBlast.jpg'
+import alt_long1 from '../../images/hilltop/alt_long1.jpg'
+import dubois_Poster from '../../images/dubois/poster.jpg'
+import whyNot from '../../images/pr/whyNot.jpg'
+
 const project_data = [
     {
         link: '/accord',
         title: 'Accord Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/accord_crop.png',
+        img: {accord_crop},
         id:'1'
       },
       {
         link: '/newburgh',
         title: 'Newburgh Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/newburgh_proj.png',
+        img: {newburgh_proj},
         id:'2'
       },
       {
         link: '/gallatin',
         title: 'Gallatin Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/gallatin/sideEleBlast.jpg',
+        img: {sideEleBlast},
         id:'3'
       },
       {
         link: '/hilltop',
         title: 'Hilltop Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/hilltop/alt_long1.jpg',
+        img: {alt_long1},
         id:'4'
       },
       {
         link: '/dubois',
         title: 'Dubois Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/dubois/poster.jpg',
+        img: {dubois_Poster},
         id:'5'
       },
       {
         link: '/pr',
         title: 'PR Passive House',
         text: '2018 PHIUS Design Competition Honorable Mention: Residential',
-        img: '../images/pr/whyNot.jpg',
+        img: {whyNot},
         id:'6'
       }
 ]
