26f92e78b24d63ba6cd37a55e71011ea1c303c21
Mark <markloercher@gmail.com>
2022-09-05 17:01:29 -0400

    added menu items back

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,54 @@
-import React, {useState } from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import Dropdown from './Dropdown'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo2 from '../assets/images/logo2.png'
 
 
 
 function NavBar() {
+
+  const [click, setClick] = useState(false)
+  const [dropdown, setDropdown] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
+  const closeMobileMenu = () => {
+    setClick(false)
+  }
+
   return (
-    <>
-    <nav className='navbar'>
+      <div className='navbar'>
         <Link to='/' className='navbar-logo'>
-        <img id="logo" src={logo2} alt='logo'></img>
+          <img id="logo" src={logo2} alt='logo'></img>
         </Link>
-    </nav>
-    </>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home 
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/About' className='nav-links' onClick={closeMobileMenu}>
+              About <i className='fas fa-caret-down' />
+            </Link>
+            {/* dropdown menu */}
+            {dropdown && <Dropdown />}
+          </li>
+          <li className='nav-item'>
+            <Link to='/About' className='nav-links' onClick={closeMobileMenu}>
+              Projects <i className='fas fa-caret-down' />
+            </Link>
+            {/* dropdown menu */}
+            {dropdown && <Dropdown />}
+          </li>
+        </ul>
+      </div>
   )
 }
 
