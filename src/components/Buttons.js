2b9f8079e0ecdd6cb2ab5c84beb4259cd1b3137e
Mark <markloercher@gmail.com>
2022-11-11 10:41:59 -0500

    what is passive house spacing fixed mobile

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Button.css'
 import { Button} from 'react-bootstrap'
 
 function Buttons({filter}) {
 
+  
+
   return (
     <div className='button-container'>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('All')} >All</Button>
+        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('All')}>All</Button>
         <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('single-family')}>Single-Family</Button>
         <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('multi-family')}>Multi-Family</Button>
         <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('commercial')}>Commercial</Button>
