028c896fc02d5d6c8e885951a7b1d510786ccac7
Mark <markloercher@gmail.com>
2022-11-22 18:33:28 -0500

    card/card container resize (less empty space around sides)

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Button.css'
-import { Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
-function Buttons({filter}) {
+function Buttons({ filter }) {
 
   return (
     <div className='button-container'>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('All')} >All</Button>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('single-family')}>Single-Family</Button>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('multi-family')}>Multi-Family</Button>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('commercial')}>Commercial</Button>
-        <Button variant="outline-light" classname='btn'type='button' onClick={()=>filter('retrofit')}>Retrofit</Button>
+      <Button variant="outline-light" classname='catBtn' onClick={() => filter('All')} >All</Button>
+      <Button variant="outline-light" classname='catBtn' onClick={() => filter('single-family')}>Single-Family</Button>
+      <Button variant="outline-light" classname='catBtn' onClick={() => filter('multi-family')}>Multi-Family</Button>
+      <Button variant="outline-light" classname='catBtn' onClick={() => filter('commercial')}>Commercial</Button>
+      <Button variant="outline-light" classname='catBtn' onClick={() => filter('retrofit')}>Retrofit</Button>
     </div>
   )
 }
